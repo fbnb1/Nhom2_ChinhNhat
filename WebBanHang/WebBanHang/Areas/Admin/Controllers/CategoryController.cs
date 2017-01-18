e0c7fa3972018bb66d1f89f1efdd3f3dbb5000a8
Chinhnd1889 <u.se7en@gmail.com>
2017-01-18 18:51:43 +0700

    Layout admin

---
+++ WebBanHang/WebBanHang/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebBanHang.Areas.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Admin/Category
+        public ActionResult List()
+        {
+            return View();
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        public ActionResult Delete()
+        {
+            return View();
+        }
+        public ActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
